eae64fc3a0d3bff0176d24a90f891ac27dac5c74
Tino Chirozvi <134059951+tinothecreator@users.noreply.github.com>
2024-09-09 23:19:13 +0200

    its in the dfs

--- Depth-FirstTraversal.cpp
+++ Depth-FirstTraversal.cpp
@@ -1,49 +1,51 @@
 #include "Depth-FirstTraversal.h"
 
 DepthFirstTraversal:: DepthFirstTraversal(const std::vector<Farm*>& farms) {
-        for (Farm* farm : farms) {
-            farmStack.push(farm);  
-        }
+    for (Farm* farm : farms) {
+        farmStack.push(farm);  
     }
+}
 
-    void DepthFirstTraversal::firstFarm()  {
-        
-    }
+void DepthFirstTraversal::firstFarm()  {
+    
+}
 
-    void DepthFirstTraversal::next()  {
-        // if (!farmStack.empty()) {
-        //     farmStack.pop(); 
-        // }
-
-        if (!farmStack.empty()) {
+void DepthFirstTraversal::next() {
+    if (!farmStack.empty()) {
         Farm* currentFarm = farmStack.top();
 
-        //  FertiliserTruck if certain conditions are met
+        if (currentFarm == nullptr) {
+            std::cerr << "Error: currentFarm is null!" << std::endl;
+            farmStack.pop(); // Pop and skip to avoid crashing
+            return;
+        }
+
         if (currentFarm->getCropType() == "Wheat") {
             FertiliserTruck* truck = new FertiliserTruck();
             truck->update(currentFarm);  
             std::cout << "FertiliserTruck dispatched for Wheat farm!" << std::endl;
         }
 
-        //  DeliveryTruck- capacity
         if (currentFarm->getTotalCapacity() > 300) {
             DeliveryTruck* truck = new DeliveryTruck();
             truck->update(currentFarm);  
             std::cout << "DeliveryTruck dispatched for farm with large capacity!" << std::endl;
         }
 
-        
+        std::cout << "here 1\n";
         farmStack.pop();
+        std::cout << "here 2\n";
     }
-    }
+}
 
-    bool DepthFirstTraversal::isDone()   {
-        return farmStack.empty();  
-    }
 
-    Farm* DepthFirstTraversal::currentFarm()   {
-        if (!isDone()) {
-            return farmStack.top();  
-        }
-        return nullptr;
+bool DepthFirstTraversal::isDone()   {
+    return farmStack.empty();  
+}
+
+Farm* DepthFirstTraversal::currentFarm()   {
+    if (!isDone()) {
+        return farmStack.top();  
     }
+    return nullptr;
+}
